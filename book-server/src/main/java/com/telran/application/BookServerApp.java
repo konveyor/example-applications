43f89d31ab76017433caa188c932cbf1b5a05463
Pranav Gaikwad <pgaikwad@redhat.com>
2023-10-19 10:14:01 -0400

    :sparkles: add book-server, seam-booking, daytrader apps
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

---
+++ book-server/src/main/java/com/telran/application/BookServerApp.java
@@ -0,0 +1,12 @@
+package com.telran.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BookServerApp.class, args);
+
+    }
+}
